db61daad946cfad99a7f8726c5a839be37340131
hollojeff <jhurst03@mail.bbk.ac.uk>
2014-07-04 16:48:26 +0100

    change Fileload from script to module
    Removed test script, added package code and renamed.

---
+++ fileload.pm
@@ -0,0 +1,16 @@
+Package Fileload;
+
+#module to load in data and add to a hash
+
+use strict;
+
+sub Fileload(){
+    
+    my %antigen;
+
+    while (my $line = <>) {
+        $line =~ /(.*),\s(.*)/;
+        $antigen{$1} = "$2";
+    }
+    return %antigen;
+}
